6735c69d5648e2123b6681cc0aeaa13b79e04173
sergei w adonev <admin@cnlist.us>
2020-04-09 14:33:43 +0300

    CSTM-9 Remove unnecessary MQ calls Removed JMS dependency Moved application.properties to proper place

--- src/main/java/us/cnlist/services/customerserver/CustomerServerApplication.java
+++ src/main/java/us/cnlist/services/customerserver/CustomerServerApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+        @PropertySource("/opt/cnlist/customserv/application.properties")
+})
 public class CustomerServerApplication extends SpringBootServletInitializer {
 
     @Override
