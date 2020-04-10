6e00aa860925e25ac42038eae31db8c143db1b85
sergei w adonev <admin@cnlist.us>
2020-04-10 16:07:33 +0300

    CSTM-10 User registration request

--- src/main/java/us/cnlist/services/customerserver/controller/UserRegistrationController.java
+++ src/main/java/us/cnlist/services/customerserver/controller/UserRegistrationController.java
@@ -1,11 +1,23 @@
 package us.cnlist.services.customerserver.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import us.cnlist.objects.requests.UserRegistrationRQ;
+import us.cnlist.objects.responses.UserRegistrationRS;
 
 @Controller
 @RequestMapping("/register")
 public class UserRegistrationController {
 
+    @PostMapping
+    @ResponseBody
+    public UserRegistrationRS receiveUserRegistrationRequest(@RequestBody UserRegistrationRQ request) {
+        UserRegistrationRS response = new UserRegistrationRS();
+        response.setUserId(-1L);
+        return response;
+    }
 
 }
