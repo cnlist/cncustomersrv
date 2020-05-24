7542eaf6fa540093eb713c832752565cc50ef86a
sergei w adonev <admin@cnlist.us>
2020-05-24 16:14:31 +0300

    LAV-27 [UI] - main page with registration form

---
+++ src/main/java/us/cnlist/services/customerserver/controller/AuthenticationController.java
@@ -0,0 +1,10 @@
+package us.cnlist.services.customerserver.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+}
